ef45baa94a97fc491e7f1c4382bd72519129507c
marmac <marmac91@gmail.com>
2014-04-07 20:35:58 +0200

    Major changes in repository structure

---
+++ build/release/moc_employee.cpp
@@ -0,0 +1,240 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'employee.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.2.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../source/employee.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'employee.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.2.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_Employee_t {
+    QByteArrayData data[30];
+    char stringdata[305];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    offsetof(qt_meta_stringdata_Employee_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData) \
+    )
+static const qt_meta_stringdata_Employee_t qt_meta_stringdata_Employee = {
+    {
+QT_MOC_LITERAL(0, 0, 8),
+QT_MOC_LITERAL(1, 9, 11),
+QT_MOC_LITERAL(2, 21, 0),
+QT_MOC_LITERAL(3, 22, 12),
+QT_MOC_LITERAL(4, 35, 11),
+QT_MOC_LITERAL(5, 47, 9),
+QT_MOC_LITERAL(6, 57, 13),
+QT_MOC_LITERAL(7, 71, 8),
+QT_MOC_LITERAL(8, 80, 11),
+QT_MOC_LITERAL(9, 92, 9),
+QT_MOC_LITERAL(10, 102, 11),
+QT_MOC_LITERAL(11, 114, 11),
+QT_MOC_LITERAL(12, 126, 8),
+QT_MOC_LITERAL(13, 135, 12),
+QT_MOC_LITERAL(14, 148, 9),
+QT_MOC_LITERAL(15, 158, 11),
+QT_MOC_LITERAL(16, 170, 8),
+QT_MOC_LITERAL(17, 179, 9),
+QT_MOC_LITERAL(18, 189, 6),
+QT_MOC_LITERAL(19, 196, 13),
+QT_MOC_LITERAL(20, 210, 10),
+QT_MOC_LITERAL(21, 221, 11),
+QT_MOC_LITERAL(22, 233, 8),
+QT_MOC_LITERAL(23, 242, 9),
+QT_MOC_LITERAL(24, 252, 6),
+QT_MOC_LITERAL(25, 259, 11),
+QT_MOC_LITERAL(26, 271, 8),
+QT_MOC_LITERAL(27, 280, 14),
+QT_MOC_LITERAL(28, 295, 5),
+QT_MOC_LITERAL(29, 301, 2)
+    },
+    "Employee\0getBirthday\0\0getFirstname\0"
+    "getLastname\0getGender\0getCompanyPtr\0"
+    "Company*\0getHiredate\0getSalary\0"
+    "getChildren\0setBirthday\0birthday\0"
+    "setFirstname\0firstname\0setLastname\0"
+    "lastname\0setGender\0gender\0setCompanyPtr\0"
+    "companyPtr\0setHiredate\0hiredate\0"
+    "setSalary\0salary\0setChildren\0children\0"
+    "QbPersistable*\0other\0id\0"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Employee[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      17,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       4,  174, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // methods: name, argc, parameters, tag, flags
+       1,    0,   99,    2, 0x02,
+       3,    0,  100,    2, 0x02,
+       4,    0,  101,    2, 0x02,
+       5,    0,  102,    2, 0x02,
+       6,    0,  103,    2, 0x02,
+       8,    0,  104,    2, 0x02,
+       9,    0,  105,    2, 0x02,
+      10,    0,  106,    2, 0x02,
+      11,    1,  107,    2, 0x02,
+      13,    1,  110,    2, 0x02,
+      15,    1,  113,    2, 0x02,
+      17,    1,  116,    2, 0x02,
+      19,    1,  119,    2, 0x02,
+      21,    1,  122,    2, 0x02,
+      23,    1,  125,    2, 0x02,
+      25,    1,  128,    2, 0x02,
+      19,    1,  131,    2, 0x00,
+
+ // methods: parameters
+    QMetaType::QDate,
+    QMetaType::QString,
+    QMetaType::QString,
+    QMetaType::QString,
+    0x80000000 | 7,
+    QMetaType::QDateTime,
+    QMetaType::Double,
+    QMetaType::Int,
+    QMetaType::Void, QMetaType::QDate,   12,
+    QMetaType::Void, QMetaType::QString,   14,
+    QMetaType::Void, QMetaType::QString,   16,
+    QMetaType::Void, QMetaType::QString,   18,
+    QMetaType::Void, 0x80000000 | 7,   20,
+    QMetaType::Void, QMetaType::QDateTime,   22,
+    QMetaType::Void, QMetaType::Double,   24,
+    QMetaType::Void, QMetaType::Int,   26,
+    QMetaType::Void, 0x80000000 | 27,   20,
+
+ // constructors: parameters
+    0x80000000 | 2,
+    0x80000000 | 2, 0x80000000 | 0,   28,
+    0x80000000 | 2, QMetaType::QDate, QMetaType::QString, QMetaType::QString, QMetaType::QString, 0x80000000 | 7, QMetaType::QDateTime, QMetaType::Double, QMetaType::Int,   12,   14,   16,   18,   20,   22,   24,   26,
+    0x80000000 | 2, QMetaType::Int, QMetaType::QDate, QMetaType::QString, QMetaType::QString, QMetaType::QString, 0x80000000 | 7, QMetaType::QDateTime, QMetaType::Double, QMetaType::Int,   29,   12,   14,   16,   18,   20,   22,   24,   26,
+
+ // constructors: name, argc, parameters, tag, flags
+       0,    0,  134,    2, 0x0e,
+       0,    1,  135,    2, 0x0e,
+       0,    8,  138,    2, 0x0e,
+       0,    9,  155,    2, 0x0e,
+
+       0        // eod
+};
+
+void Employee::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::CreateInstance) {
+        switch (_id) {
+        case 0: { Employee *_r = new Employee();
+            if (_a[0]) *reinterpret_cast<QObject**>(_a[0]) = _r; } break;
+        case 1: { Employee *_r = new Employee((*reinterpret_cast< const Employee(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast<QObject**>(_a[0]) = _r; } break;
+        case 2: { Employee *_r = new Employee((*reinterpret_cast< QDate(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2])),(*reinterpret_cast< QString(*)>(_a[3])),(*reinterpret_cast< QString(*)>(_a[4])),(*reinterpret_cast< Company*(*)>(_a[5])),(*reinterpret_cast< QDateTime(*)>(_a[6])),(*reinterpret_cast< double(*)>(_a[7])),(*reinterpret_cast< int(*)>(_a[8])));
+            if (_a[0]) *reinterpret_cast<QObject**>(_a[0]) = _r; } break;
+        case 3: { Employee *_r = new Employee((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< QDate(*)>(_a[2])),(*reinterpret_cast< QString(*)>(_a[3])),(*reinterpret_cast< QString(*)>(_a[4])),(*reinterpret_cast< QString(*)>(_a[5])),(*reinterpret_cast< Company*(*)>(_a[6])),(*reinterpret_cast< QDateTime(*)>(_a[7])),(*reinterpret_cast< double(*)>(_a[8])),(*reinterpret_cast< int(*)>(_a[9])));
+            if (_a[0]) *reinterpret_cast<QObject**>(_a[0]) = _r; } break;
+        }
+    } else if (_c == QMetaObject::InvokeMetaMethod) {
+        Employee *_t = static_cast<Employee *>(_o);
+        switch (_id) {
+        case 0: { QDate _r = _t->getBirthday();
+            if (_a[0]) *reinterpret_cast< QDate*>(_a[0]) = _r; }  break;
+        case 1: { QString _r = _t->getFirstname();
+            if (_a[0]) *reinterpret_cast< QString*>(_a[0]) = _r; }  break;
+        case 2: { QString _r = _t->getLastname();
+            if (_a[0]) *reinterpret_cast< QString*>(_a[0]) = _r; }  break;
+        case 3: { QString _r = _t->getGender();
+            if (_a[0]) *reinterpret_cast< QString*>(_a[0]) = _r; }  break;
+        case 4: { Company* _r = _t->getCompanyPtr();
+            if (_a[0]) *reinterpret_cast< Company**>(_a[0]) = _r; }  break;
+        case 5: { QDateTime _r = _t->getHiredate();
+            if (_a[0]) *reinterpret_cast< QDateTime*>(_a[0]) = _r; }  break;
+        case 6: { double _r = _t->getSalary();
+            if (_a[0]) *reinterpret_cast< double*>(_a[0]) = _r; }  break;
+        case 7: { int _r = _t->getChildren();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = _r; }  break;
+        case 8: _t->setBirthday((*reinterpret_cast< QDate(*)>(_a[1]))); break;
+        case 9: _t->setFirstname((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 10: _t->setLastname((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 11: _t->setGender((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 12: _t->setCompanyPtr((*reinterpret_cast< Company*(*)>(_a[1]))); break;
+        case 13: _t->setHiredate((*reinterpret_cast< QDateTime(*)>(_a[1]))); break;
+        case 14: _t->setSalary((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 15: _t->setChildren((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 16: _t->setCompanyPtr((*reinterpret_cast< QbPersistable*(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 12:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< Company* >(); break;
+            }
+            break;
+        case 16:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QbPersistable* >(); break;
+            }
+            break;
+        }
+    }
+}
+
+const QMetaObject Employee::staticMetaObject = {
+    { &QbPersistable::staticMetaObject, qt_meta_stringdata_Employee.data,
+      qt_meta_data_Employee,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *Employee::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Employee::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_Employee.stringdata))
+        return static_cast<void*>(const_cast< Employee*>(this));
+    return QbPersistable::qt_metacast(_clname);
+}
+
+int Employee::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QbPersistable::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 17)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 17;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 17)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 17;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
